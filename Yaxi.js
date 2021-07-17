6f8bb1820366e51d8af1b37f104c7e6553cf9da7
Necon123 <86918032+Necon123@users.noreply.github.com>
2021-07-17 15:28:40 +0100

    Update Yaxi.js

--- Yaxi.js
+++ Yaxi.js
@@ -91,7 +91,7 @@ client.on("message", msg => {
 });
 /////Reklam////
 client.on('ready' , () => {
-var join = client.channels.get("id voice")/// ئاید ڤۆیس
+var join = client.channels.get("861210405376753704")/// ئاید ڤۆیس
 if (join) join.join()
 })
 
@@ -101,14 +101,14 @@ if(message.channel.type === 'dm') {
  
 if (message.content.startsWith("https://discord.gg/")) {
  
-  message.author.send(`> link server`)/// لینکە سیرڤەرە خۆت
+  message.author.send(`> https://discord.gg/6jDDPTkcBM`)/// لینکە سیرڤەرە خۆت
  
-  client.channels.get("id text").send(
+  client.channels.get("861210373745803264").send(
 `> Nerdra la layan <@${message.author.id}> 
 ${message.content}`)
 }
     }
         })
 
-client.login("");
+client.login("ODY1OTU5ODA3MjQ4MTA1NDcy.YPLl6g.vFJLZFan5i1zxU5H4tfYZrPKbCU");
 
